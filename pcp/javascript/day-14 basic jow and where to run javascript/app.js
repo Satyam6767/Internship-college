7fbac5c88a630963c19eeb60e372f1c49605b148
Satyam6767 <satyamkumar6767@gmail.com>
2025-06-21 09:14:15 +0530

    first commit

---
+++ pcp/javascript/day-14 basic jow and where to run javascript/app.js
@@ -0,0 +1,19 @@
+// let a = 123421354;
+// let b = 8753284;
+// let c = "abcd ";
+
+
+
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+
+// console.log(c)
+// console.log(typeof c)
+
+
+let answer = false;
+console.log(answer);
+
+console.log(typeof(answer))
---
+++ yb/day-14 javascript basic, how and where to run/app.js
@@ -0,0 +1,10 @@
+const  a = "123432";
+const  b = 8767656;
+
+
+console.log(a+b)
+console.log(a*b)
+
+
+console.log(typeof a)
+console.log(typeof(a))
