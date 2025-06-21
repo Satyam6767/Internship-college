e03d19961d311b5c6f8ab48e06093d0600fe1c72
Satyam6767 <satyamkumar6767@gmail.com>
2025-06-21 10:09:22 +0530

    initial commit

---
+++ pcp/javascript/day-15/app.js
@@ -0,0 +1,46 @@
+let fname = "satyam"
+let lname = "kumar"
+let address = "pune"
+
+// my first name is satyam and my last name is kumar. my address is pune
+
+console.log("my first name is " + fname + " and my last name is " +  lname +
+     " and my address is " + address)
+
+
+// backticks/template literals
+console.log(`my first name is ${fname} and my last name is ${lname} and my 
+    address is ${address}`)
+
+
+
+
+
+
+
+// let age = "10";
+// let age2 = 15
+
+
+
+
+// string => + => conccatination
+
+
+// number => + => sum
+
+
+
+
+
+
+
+// datatype => string
+// let num1 = "60";
+// datatype => number   
+// let num2 = 29;
+
+
+// let c = num1+num2;
+// console.log(typeof c)
+
---
+++ pcp/javascript/day-15/app2.js
@@ -0,0 +1,25 @@
+// var age = 30;
+// console.log(age)
+
+// var age = 20;
+// console.log(age)
+
+
+
+
+
+
+let age = 30;
+console.log(age)
+ age = 40;
+
+
+
+
+// const age = 30;
+// console.log(age)
+
+// age = 40;
+// console.log(age)
+
+
