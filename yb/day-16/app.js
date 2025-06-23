191e8d49a6170c21f17c954235307bcf31346d3b
Satyam6767 <satyamkumar6767@gmail.com>
2025-06-23 11:33:19 +0530

    initial-commit

---
+++ yb/day-16/app.js
@@ -0,0 +1,77 @@
+// var student_name1 = "xyz";
+// var student_name2 = "abc";
+// var student_name3 = "qwe";
+
+// console.log(student_name1)
+// index number      0,     1,     2     3
+// var all_students = ["xyz","abc", "qwe", 87]
+// console.log(all_students);
+// console.log(all_students[2])
+
+
+
+
+// var age = [20, 30, 40, 20, 30, 40,30, 40, 20, 30, 40, 50,20,
+//      30, 40, 20, 30, 40,30, 40, 20, 30, 40, 50, 60, 65, 50, 60, 
+//      65, 50, 60, 65, "abc", 100]
+
+// var result = age.length-1
+// console.log(age[result])
+
+
+
+
+// push 
+
+// var data = [29, 27, 37, 34, 16];
+// console.log(data)
+// data.push(55)
+// console.log(data)
+
+
+
+
+
+// pop 
+
+// var data = [29, 27, 37, 34, 16];
+// console.log(data)
+// data.pop()
+// console.log(data)
+
+
+
+// unshift 
+
+// var data = [29, 27, 37, 34, 16];
+// console.log(data)
+// data.unshift(100)
+// console.log(data)
+
+
+
+// shift 
+
+// var data = [29, 27, 37, 34, 16];
+// console.log(data)
+// data.shift()
+// console.log(data)
+
+
+
+
+
+// slice
+
+var data = [29, 27, 37, 34, 16, 12, 14, 11, 10,22, 40, 56, 78, 86, 96, 64, 67];
+console.log(data)
+console.log(data.slice(0,5))
+console.log(data.slice(5, 10))
+
+
+
+
+
+
+
+
